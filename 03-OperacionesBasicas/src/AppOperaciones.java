0039c1e19dbe472fcc3fa7d71daf0e996e095af0
Jonathan Valdez Gayosso <j_valdezg@hotmail.com>
2016-01-15 12:42:41 -0600

    03-OperacionesBasicas
    03-OperacionesBasicas

---
+++ 03-OperacionesBasicas/src/AppOperaciones.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Jonathan Valdez Gayosso
+ *
+ */
+public class AppOperaciones {
+
+	/**
+	 * 
+	 * @param Ventana
+	 */
+	public static void main(String[] args) {
+		Ventana vtn= new Ventana();
+		vtn.setVisible(true);
+	}
+
+}
---
+++ 03-OperacionesBasicas/src/Operaciones.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @author Jonathan Valdez Gayosso
+ *
+ */
+public class Operaciones {
+	public int sumar(int n1, int n2){
+		return n1 + n2;
+	}
+}
