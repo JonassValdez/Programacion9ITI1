9f11e5922bef7fee9d18c818d9d9c5694650a7f7
Jonathan Valdez Gayosso <j_valdezg@hotmail.com>
2016-01-15 12:38:46 -0600

    00-Saludo
    00-Saludo

---
+++ 00-saludo/src/Saludo.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Jonathan Valdez Gayosso
+ *
+ */
+public class Saludo {
+	
+	/**
+	 * 
+	 * @param mi nombre
+	 */
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Jonathan Valdez Gayosso ");
+		System.out.print("Jonathan Valdez Gayosso ");
+
+	}
+
+}
